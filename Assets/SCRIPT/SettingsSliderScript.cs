dae762f83e3318774232082eef53c1a7a907a3a3
pineapplelimejelly <eva.a.y.tsai@gmail.com>
2023-04-01 12:42:08 -0400

    Slider script added.

--- Assets/SCRIPT/SettingsSliderScript.cs
+++ Assets/SCRIPT/SettingsSliderScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class SettingsSliderScript : MonoBehaviour
 {
+    [SerializeField] private Slider _slider;
+
+    [SerializeField] private TextMeshProUGUI _sliderText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _slider.onValueChanged.AddListener((v) => {
+            _sliderText.text = v.ToString("0.00");
+        });
     }
 
     // Update is called once per frame
